63950f537ec9bf798805a0862ce82c190267e7b4
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2021-05-26 14:56:58 -0500

    Update inicio.php

--- ca_cotecnova/assets/pages/inicio.php
+++ ca_cotecnova/assets/pages/inicio.php
@@ -1,8 +1,8 @@
-<div class="col-md-offset-2 col-md-8 col-sm-8">
+<div style="padding-top: 150px" class="col-md-offset-2 col-md-8 col-sm-8">
   <div class="panel panel-danger  text-center">
       
       <div class="panel-body">
-          Aquí puedes transferir dinero a otras cuentas que tenga vínculo con Web Bank.
+      En el panel izquierdo de este espacio usted podra encontrar el acceso de toda la informacion acedemica que le corresponde, como Administrador puede hacer todas las operaciones disponibles, si desea agregar, modificar, eliminar o buscar un dato recuerde hacerlo desde el correspondiente modulo.
       </div>
       
   </div>
