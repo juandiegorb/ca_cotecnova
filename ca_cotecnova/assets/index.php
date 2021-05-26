4bd5a5b1b4c4d4d8953a88857ab4b58f76c6ff30
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2021-05-26 16:55:02 -0500

    Update index.php

--- ca_cotecnova/assets/index.php
+++ ca_cotecnova/assets/index.php
@@ -80,17 +80,7 @@
                 <div class="navbar-default sidebar" role="navigation">
                     <div class="sidebar-nav navbar-collapse">
                         <ul class="nav" id="side-menu">
-                            <li class="sidebar-search">
-                                <div class="input-group custom-search-form">
-                                    <input type="text" class="form-control" placeholder="Search...">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                </span>
-                                </div>
-                                <!-- /input-group -->
-                            </li>
+                            
                             <li>
                                 <a href="#" id="inicio" class="active"><i class="fa fa-home fa-fw"></i> Inicio</a>
                             </li>
@@ -113,9 +103,6 @@
                             </li>
                             <li>
                                 <a href="#"><i class="fa fa-edit fa-fw"></i> Gestionar Carrera</a>
-                            </li>
-                            <li>
-                                <a id="cerrar_session"><i class="fa fa-user fa-fw"></i> Cerrar Sesion</a>
                             </li>
                         </ul>
                     </div>
